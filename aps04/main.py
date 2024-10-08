2a4bec49761438c633077b9653a07e798b768f3e
luabatepietro <luabatepietroapple@icloud.com>
2024-10-07 23:54:03 -0300

    fix total

--- aps04/main.py
+++ aps04/main.py
@@ -7,13 +7,11 @@ pygame.init()
 largura, altura = 800, 600
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("APS04")
-
 preto = (0, 0, 0)
 cor = (255, 50, 133)
 
-
-#me senti fazendo uma tabela verdade
-vertices_cubo = np.array([
+# me senti fazendo uma tabela verdade
+vert_cubo = np.array([
     [-1, -1, -1],
     [-1, -1,  1],
     [-1,  1, -1],
@@ -23,125 +21,109 @@ vertices_cubo = np.array([
     [ 1,  1, -1],
     [ 1,  1,  1]
 ]) * 100
+arestas_cubo = [(0, 1), (1, 3), (3, 2), (2, 0), (4, 5), (5, 7), (7, 6), (6, 4), (0, 4), (1, 5), (2, 6), (3, 7)]
+
 
 #tabela verdade pt2.0
-vertices_tetraedro = np.array([
+vert_tri = np.array([
     [1, 1, 1],
     [-1, -1, 1],
     [-1, 1, -1],
     [1, -1, -1]
 ]) * 100
+arestas_triangulo = [(0, 1), (1, 2), (2, 0), (0, 3), (1, 3), (2, 3)]
 
-arestas_cubo = [(0, 1), (1, 3), (3, 2), (2, 0), (4, 5), (5, 7), (7, 6), (6, 4), (0, 4), (1, 5), (2, 6), (3, 7)]
-arestas_tetraedro = [(0, 1), (1, 2), (2, 0), (0, 3), (1, 3), (2, 3)]
 
-def proj_ponto(ponto, dist):
-    if ponto[2] != 0:
-        fator_proj = dist / (dist - ponto[2])
-    else:
-        fator_proj = 1
-    x = ponto[0] * fator_proj + largura // 2
-    y = ponto[1] * fator_proj + altura // 2
-    return [int(x), int(y)]
 
 def rotacao(pts, angulo_x, angulo_y, angulo_z):
-    rot_x = np.array([[1, 0, 0],
-                      [0, math.cos(angulo_x), -math.sin(angulo_x)],
-                      [0, math.sin(angulo_x), math.cos(angulo_x)]])
+    Rx = np.array([[1, 0, 0],
+                   [0, math.cos(angulo_x), -math.sin(angulo_x)],
+                   [0, math.sin(angulo_x), math.cos(angulo_x)]])
+    Ry = np.array([[math.cos(angulo_y), 0, math.sin(angulo_y)],
+                   [0, 1, 0],
+                   [-math.sin(angulo_y), 0, math.cos(angulo_y)]])
+    Rz = np.array([[math.cos(angulo_z), -math.sin(angulo_z), 0],
+                   [math.sin(angulo_z), math.cos(angulo_z), 0],
+                   [0, 0, 1]])
     
-    rot_y = np.array([[math.cos(angulo_y), 0, math.sin(angulo_y)],
-                      [0, 1, 0],
-                      [-math.sin(angulo_y), 0, math.cos(angulo_y)]])
-    
-    rot_z = np.array([[math.cos(angulo_z), -math.sin(angulo_z), 0],
-                      [math.sin(angulo_z), math.cos(angulo_z), 0],
-                      [0, 0, 1]])
+    return np.dot(np.dot(np.dot(pts, Rx), Ry), Rz) #tudo em uma linha XD
 
+'''
+nao funciona de jeito nenhum a projeção da forma "correta" tive que fazer uma matriz um pouco diferente, 
+talvez tenha feito besteira antes.
+'''
+def projecao_ponto(vertice, d):
+    P = np.array([
+        [0, 0, -d],
+        [1, 0, 0],
+        [0, 1/d, 0]
+    ])
     
-    #tudo em uma matriz só XD
-    M = np.dot(np.dot(rot_x, rot_y), rot_z)
-    
-    rot_pts = np.dot(pts, M)
-    
-    return rot_pts
+    ponto_proj = np.dot(P, vertice)
+    return (largura // 2 + int(ponto_proj[1]), altura // 2 - int(ponto_proj[2]))
+
 
 angulo_x, angulo_y, angulo_z = 0, 0, 0
-velocidade_rot = 0.003
-dist_proj = 500
-
+v_rot = 0.01
+rot = 0.005  
+dis = 2
 mov_x, mov_y, mov_z = 0, 0, 0
-mov_velocidade = 10
-
+mov_v = 10
 forma_atual = 'cubo' 
 
 rodando = True
 while rodando:
     tela.fill(preto)
-    
     for evento in pygame.event.get():
-
         if evento.type == pygame.QUIT:
             rodando = False
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_t:
-                forma_atual = 'tetraedro' if forma_atual == 'cubo' else 'cubo' #trocar entre os dois de forma bacana!!
-    
+                forma_atual = 'triangulo' if forma_atual == 'cubo' else 'cubo'  
 
     teclas = pygame.key.get_pressed()
 
-    #fazer rodar doidamente
     if teclas[pygame.K_LEFT]:
-        angulo_y -= 0.02
+        angulo_y -= v_rot
     if teclas[pygame.K_RIGHT]:
-        angulo_y += 0.02
+        angulo_y += v_rot
     if teclas[pygame.K_UP]:
-        angulo_x -= 0.02
+        angulo_x -= v_rot
     if teclas[pygame.K_DOWN]:
-        angulo_x += 0.02
+        angulo_x += v_rot
+    if teclas[pygame.K_q]:
+        angulo_z -= v_rot
+    if teclas[pygame.K_e]:
+        angulo_z += v_rot
 
-
-    #"andar" 
-    if teclas[pygame.K_a]:
-        mov_x -= mov_velocidade
-    if teclas[pygame.K_d]:
-        mov_x += mov_velocidade
     if teclas[pygame.K_w]:
-        mov_z += mov_velocidade
+        mov_y += mov_v
     if teclas[pygame.K_s]:
-        mov_z -= mov_velocidade
+        mov_y -= mov_v
+    if teclas[pygame.K_a]:
+        mov_x -= mov_v
+    if teclas[pygame.K_d]:
+        mov_x += mov_v
 
-    #cubo ou triangulo
+    angulo_x += rot
+    angulo_y += rot
+
     if forma_atual == 'cubo':
-        vertices = vertices_cubo
+        vertices = vert_cubo
         arestas = arestas_cubo
     else:
-        vertices = vertices_tetraedro
-        arestas = arestas_tetraedro
+        vertices = vert_tri
+        arestas = arestas_triangulo
 
-    forma_rot = rotacao(vertices, angulo_x, angulo_y, angulo_z)
+    vertices_rotacionados = rotacao(vertices, angulo_x, angulo_y, angulo_z)
     
-    pontos_proj = []
-    for vert in forma_rot:
-        vert[0] += mov_x
-        vert[1] += mov_y
-        vert[2] += mov_z
-        pontos_proj.append(proj_ponto(vert, dist_proj))
-    
+    pontos_proj = [projecao_ponto(v + [mov_x, mov_y, mov_z], dis) for v in vertices_rotacionados]
     for aresta in arestas:
-        pygame.draw.line(tela, cor, pontos_proj[aresta[0]], pontos_proj[aresta[1]], 3)
+        pygame.draw.line(tela, cor, pontos_proj[aresta[0]], pontos_proj[aresta[1]], 2)
     
-    for vertice in pontos_proj:
-        pygame.draw.circle(tela, cor, vertice, 5)
-    
-    angulo_x += velocidade_rot
-    angulo_y += velocidade_rot
-    angulo_z += velocidade_rot
-
-    angulo_x %= 2 * math.pi
-    angulo_y %= 2 * math.pi
-    angulo_z %= 2 * math.pi
-
     pygame.display.flip()
+
+    # se tirar isso ele fica girando que nem um maluco
     pygame.time.wait(10)
 
 pygame.quit()
